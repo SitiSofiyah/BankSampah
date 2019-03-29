6a8fd3055d3620592410e2bfdef639780320835c
Zulfiyah <45227497+Zulfiyah@users.noreply.github.com>
2019-03-29 14:19:40 +0700

    tambaah grup

---
+++ FireApp/app/src/main/java/com/example/intel/fireapp/Anggota/Infoamasi.java
@@ -0,0 +1,15 @@
+package com.example.intel.fireapp.Anggota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.intel.fireapp.R;
+
+public class Infoamasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infoamasi);
+    }
+}
