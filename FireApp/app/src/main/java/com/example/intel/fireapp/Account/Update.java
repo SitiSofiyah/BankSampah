fbc7a336c3c78d3cb780e750cd97b43124feec02
Zulfiyah <45227497+Zulfiyah@users.noreply.github.com>
2019-04-29 13:27:32 +0700

    ddhfj

--- FireApp/app/src/main/java/com/example/intel/fireapp/Account/Update.java
+++ FireApp/app/src/main/java/com/example/intel/fireapp/Account/Update.java
@@ -1,9 +1,12 @@
 package com.example.intel.fireapp.Account;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,6 +24,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+
+import static android.text.TextUtils.isEmpty;
 
 public class Update extends AppCompatActivity {
 
@@ -44,49 +49,42 @@ public class Update extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getInstance().getReference();
 
         final Query query = mDatabase.child("users").orderByChild("id").equalTo(SaveSharedPreference.getId(getApplicationContext()));
-        query.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
-                User akun = dataSnapshot.getValue(User.class);
-
-                        ed_Nama.setText(akun.getNama().toString());
-                        ed_Alamat.setText(akun.getAlamat().toString());
-                        ed_Telp.setText(akun.getTelp().toString());
-                        ed_Password.setText(akun.getPassword().toString());
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(getApplicationContext(),"Canceled",Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
-        bt_update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        final User user = (User) getIntent().getSerializableExtra("data");
+        if (user != null) {
+            ed_Nama.setText(user.getNama());
+            ed_Password.setText(user.getPassword());
+            ed_Alamat.setText(user.getAlamat());
+            ed_Telp.setText(user.getTelp());
 
-            }
-        });
+            bt_update.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    user.setNama(ed_Nama.getText().toString());
+//                    user.setPassword(etMerk.getText().toString());
+//                    user.setAlamat(etHarga.getText().toString());
+//
+//                    updateBarang(barang);
+                }
+            });
+            bt_update.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!isEmpty(ed_Nama.getText().toString()) && !isEmpty(ed_Password.getText().toString()) && !isEmpty(ed_Alamat.getText().toString()) && !isEmpty(ed_Telp.getText().toString()));
+//                        submitUsers(new users(ed_Nama.getText().toString(), ed_Password.getText().toString(), ed_Alamat.getText().toString(), ed_Telp.getText().toString()));
+                    else
+                    Snackbar.make(findViewById(R.id.update), "Data barang tidak boleh kosong", Snackbar.LENGTH_LONG).show();
+
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(
+                            ed_Nama.getWindowToken(), 0);
+                }
+            });
+        }
 
 
     }
-
-
 }
+
