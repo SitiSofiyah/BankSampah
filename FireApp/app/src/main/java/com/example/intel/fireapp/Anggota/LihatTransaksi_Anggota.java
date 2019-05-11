5d54a23fdb3c8f4a3b086730799af1751cb1aa92
Zulfiyah <45227497+Zulfiyah@users.noreply.github.com>
2019-05-11 20:52:06 +0700

    b

---
+++ FireApp/app/src/main/java/com/example/intel/fireapp/Anggota/LihatTransaksi_Anggota.java
@@ -0,0 +1,15 @@
+package com.example.intel.fireapp.Anggota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.intel.fireapp.R;
+
+public class LihatTransaksi_Anggota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_transaksi__anggota);
+    }
+}
