f4eebf5a6d3d7a990758cd10107567afc5d1058b
Zulfiyah <45227497+Zulfiyah@users.noreply.github.com>
2019-05-17 08:04:38 +0700

    ksj

--- FireApp/app/src/main/java/com/example/intel/fireapp/Anggota/Home_Anggota.java
+++ FireApp/app/src/main/java/com/example/intel/fireapp/Anggota/Home_Anggota.java
@@ -1,10 +1,16 @@
 package com.example.intel.fireapp.Anggota;
 
 import android.content.Intent;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.intel.fireapp.Account.Utils.SaveSharedPreference;
+import com.example.intel.fireapp.Account.login;
 import com.example.intel.fireapp.PengepulKecil.HomePK;
 import com.example.intel.fireapp.PengepulKecil.tambahgrup;
 import com.example.intel.fireapp.R;
@@ -15,6 +21,44 @@ public class Home_Anggota extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home__anggota);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Tabungan Sampah");
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.akun:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.help:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            case R.id.out:
+                SaveSharedPreference.setLoggedInPK(getApplicationContext(), false);
+                SaveSharedPreference.setId(getApplicationContext(), null);
+                Intent intent = new Intent(Home_Anggota.this, login.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     public void info(View view) {
