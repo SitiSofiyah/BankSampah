233bf96416c6caca78a266c01a7b2ece150980eb
SitiSofiyah <sitisofia05@gmail.com>
2019-05-20 19:54:46 +0700

    desain homepk

--- FireApp/app/src/main/java/com/example/intel/fireapp/PengepulKecil/HomePK.java
+++ FireApp/app/src/main/java/com/example/intel/fireapp/PengepulKecil/HomePK.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.support.v7.widget.Toolbar;
 
+import com.example.intel.fireapp.Account.Bantuan;
 import com.example.intel.fireapp.Account.Utils.SaveSharedPreference;
 import com.example.intel.fireapp.Account.login;
 import com.example.intel.fireapp.Model.Tawaran;
@@ -24,7 +25,6 @@ import com.nex3z.notificationbadge.NotificationBadge;
 
 public class HomePK extends AppCompatActivity {
 
-    private Button btgrup;
     NotificationBadge nBadge;
     private DatabaseReference ref = FirebaseDatabase.getInstance().getReference("transaksiTR");
 
@@ -32,68 +32,10 @@ public class HomePK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_pk);
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("Tabungan Sampah");
-        setSupportActionBar(toolbar);
-
-        nBadge  = (NotificationBadge) findViewById(R.id.badge);
-
-        ref.addValueEventListener(new ValueEventListener() {
-
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
-                   nBadge.setNumber((int) dataSnapshot.getChildrenCount());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
 
     }
 
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.akun:
-                // User chose the "Settings" item, show the app settings UI...
-                Intent intents = new Intent(HomePK.this, db_ReadAkun.class);
-                startActivity(intents);
-                return true;
-
-            case R.id.help:
-                // User chose the "Favorite" action, mark the current item
-                // as a favorite...
-                return true;
-
-            case R.id.out:
-                SaveSharedPreference.setLoggedInPK(getApplicationContext(), false);
-                SaveSharedPreference.setId(getApplicationContext(), null);
-                Intent intent = new Intent(HomePK.this, login.class);
-                startActivity(intent);
-                finish();
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
-    }
     public void grup(View view) {
         Intent intent = new Intent(HomePK.this,tambahgrup.class);
         startActivity(intent);
@@ -101,6 +43,24 @@ public class HomePK extends AppCompatActivity {
 
     public void order(View view) {
         Intent intent = new Intent(HomePK.this,transaksikeTR.class);
+        startActivity(intent);
+    }
+
+    public void bantuan(View view) {
+        Intent intent = new Intent(HomePK.this,Bantuan.class);
+        startActivity(intent);
+    }
+
+    public void logout(View view) {
+        SaveSharedPreference.setLoggedInPK(this,false);
+        SaveSharedPreference.setId(this,null);
+        Intent intent = new Intent(HomePK.this,login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void akun(View view) {
+        Intent intent = new Intent(HomePK.this,db_ReadAkun.class);
         startActivity(intent);
     }
 
